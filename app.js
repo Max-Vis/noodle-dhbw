823d7ab11f70520ec090007337c6d4d3c09f95e1
Maximilian Roth <wi22266@lehre.dhbw-stuttgart.de>
2023-10-10 13:19:05 +0200

    NavMenu (gemacht)

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const path = require('path')
 
 const app = express();
+app.use(express.static(__dirname + '/views'));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
+app.set('/views')
 app.use(
     express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
   );
